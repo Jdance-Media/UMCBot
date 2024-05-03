803a5b045fdceb3a491a1a6fae0a9f685703bde0
Jdance-Media <67397422+Jdance-Media@users.noreply.github.com>
2024-05-03 17:52:16 -0400

    Fixed up the bot. added some touches.

---
+++ UMCBot/Models/UMCBotDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMCBot.Models
+{
+    public class UMCBotDbContext : DbContext
+    {
+        public DbSet<Application> Applications { get; set; } = null!;
+
+        private readonly string _dbPath;
+
+        public UMCBotDbContext()
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            _dbPath = Path.Join(path, "UMCBot.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite($"Data Source={_dbPath}");
+
+        }
+    }
+
+    public class Application
+    {
+        [Key]
+        public ulong UserId { get; set; }
+        public ulong[]? Votes { get; set; }
+    }
+}
--- UMCBot/Modules/ApplicationCommandModule.cs
+++ UMCBot/Modules/ApplicationCommandModule.cs
@@ -1,10 +1,14 @@
 ﻿using Discord;
 using Discord.Interactions;
+using GenHTTP.Modules.IO;
+using Microsoft.EntityFrameworkCore;
+using Pterodactyl.NET.Objects.V0_7.Admin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UMCBot.Models;
 
 namespace UMCBot.Modules;
 
@@ -12,6 +16,10 @@ namespace UMCBot.Modules;
 public class ApplicationCommandModule : InteractionModuleBase
 {
     public const ulong ApplicationChannel = 0;
+
+    public const ulong Role = 0;
+
+    public const uint VotesRequired = 4;
 
     //This is a note put on the test branch.
 
@@ -24,6 +32,16 @@ public class ApplicationCommandModule : InteractionModuleBase
     [ModalInteraction("umc_application")]
     public async Task ApplicationModalResponse(ApplicationModal modal)
     {
+        await using var dbContext = new UMCBotDbContext();
+
+        var application = dbContext.Applications.Find(Context.User.Id);
+        if (application != null)
+        {
+            await RespondAsync("You already have an application submitted.", ephemeral: true);
+            return;
+        }
+        dbContext.Applications.Add(new Application() { UserId = Context.User.Id, Votes = new ulong[] { } });
+
         var applicationChannel = (ITextChannel)(await Context.Guild.GetChannelAsync(ApplicationChannel));
 
         var embed = new EmbedBuilder()
@@ -32,13 +50,71 @@ public class ApplicationCommandModule : InteractionModuleBase
             .WithDescription($"**User:**\n{Context.User.GlobalName}\n\n**Reason:**\n{modal.Reason}\n\n**Previous Work:**\n{modal.PreviousWork}\n\n**Invite Explanation:**{modal.InviteExplanation}")
             .Build();
 
-        await applicationChannel.SendMessageAsync(embed: embed, components: new ComponentBuilder().WithButton("Approve", "button_approve", ButtonStyle.Primary, new Emoji("✔")).Build());
+        await applicationChannel.SendMessageAsync(embed: embed, components: new ComponentBuilder().WithButton("Approve", $"button_approve:{Context.User.Id}", ButtonStyle.Primary, new Emoji("✔")).Build());
+
+        
     }
 
-    [ComponentInteraction("button_approve")]
-    public async Task ApplicationApprovalResponse()
+    [ComponentInteraction("button_approve:*")]
+    public async Task ApplicationApprovalResponse(string id)
     {
+        if (!ulong.TryParse(id, out var userId))
+        {
+            await RespondAsync("Specified application could not be found", ephemeral: true);
+            return;
+        }
+        var user = Context.Guild.GetUserAsync(userId);
+        if (user == null)
+        {
+            await RespondAsync("Specified application could not be found", ephemeral: true);
+            return;
+        }
+        await DeferAsync(true);
 
+        await using var dbContext = new UMCBotDbContext();
+
+        var application = dbContext.Applications.Find(userId);
+        if (application == null)
+        {
+            await RespondAsync("Specified application could not be found", ephemeral: true);
+            return;
+        }
+        
+        var votes = application.Votes!.ToList();
+        if (votes.Contains(Context.User.Id))
+        {
+            votes.Remove(Context.User.Id);
+            await RespondAsync("Removed your vote.", ephemeral: true);
+        }
+        else
+        {
+            votes.Add(Context.User.Id);
+            await RespondAsync("Added your vote.", ephemeral: true);
+        }
+        application.Votes = votes.ToArray();
+
+        await dbContext.SaveChangesAsync();
+
+        await CheckApplication(userId);
+        return;
+    }
+
+    public async Task CheckApplication(ulong id)
+    {
+        await using var dbContext = new UMCBotDbContext();
+
+        var application = dbContext.Applications.Find(id);
+        if (application == null)
+            return;
+
+        if (application.Votes!.Length >= VotesRequired)
+        {
+            var user = await Context.Guild.GetUserAsync(id);
+            if (user != null)
+                await user.AddRoleAsync(Role);
+        }
+
+        dbContext.Applications.Remove(application);
     }
 }
 
